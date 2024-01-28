3a855194d4e1a11e638825aafdbc4d80e87d85a4
KrzzLpzz <crisam1801@gmail.com>
2024-01-27 23:14:08 -0600

    Agregar archivos de proyecto.

---
+++ CastingConversion/MainForm.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtAltura = New System.Windows.Forms.TextBox()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblApellido = New System.Windows.Forms.Label()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
+        Me.lblEdad = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.lblAltura = New System.Windows.Forms.Label()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.rtbResultado = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(134, 127)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(75, 23)
+        Me.btnIngresar.TabIndex = 4
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtAltura)
+        Me.GroupBox1.Controls.Add(Me.btnIngresar)
+        Me.GroupBox1.Controls.Add(Me.lblNombre)
+        Me.GroupBox1.Controls.Add(Me.lblApellido)
+        Me.GroupBox1.Controls.Add(Me.txtEdad)
+        Me.GroupBox1.Controls.Add(Me.lblEdad)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.txtApellido)
+        Me.GroupBox1.Controls.Add(Me.lblAltura)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 14)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(342, 169)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese sus datos:"
+        '
+        'txtAltura
+        '
+        Me.txtAltura.Location = New System.Drawing.Point(129, 97)
+        Me.txtAltura.Name = "txtAltura"
+        Me.txtAltura.Size = New System.Drawing.Size(189, 20)
+        Me.txtAltura.TabIndex = 3
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(25, 22)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(97, 13)
+        Me.lblNombre.TabIndex = 0
+        Me.lblNombre.Text = "Ingrese su nombre:"
+        '
+        'lblApellido
+        '
+        Me.lblApellido.AutoSize = True
+        Me.lblApellido.Location = New System.Drawing.Point(25, 48)
+        Me.lblApellido.Name = "lblApellido"
+        Me.lblApellido.Size = New System.Drawing.Size(98, 13)
+        Me.lblApellido.TabIndex = 3
+        Me.lblApellido.Text = "Ingrese su apellido:"
+        '
+        'txtEdad
+        '
+        Me.txtEdad.Location = New System.Drawing.Point(129, 71)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(189, 20)
+        Me.txtEdad.TabIndex = 2
+        '
+        'lblEdad
+        '
+        Me.lblEdad.AutoSize = True
+        Me.lblEdad.Location = New System.Drawing.Point(25, 74)
+        Me.lblEdad.Name = "lblEdad"
+        Me.lblEdad.Size = New System.Drawing.Size(86, 13)
+        Me.lblEdad.TabIndex = 4
+        Me.lblEdad.Text = "Ingrese su edad:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(129, 19)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(189, 20)
+        Me.txtNombre.TabIndex = 0
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(129, 45)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(189, 20)
+        Me.txtApellido.TabIndex = 1
+        '
+        'lblAltura
+        '
+        Me.lblAltura.AutoSize = True
+        Me.lblAltura.Location = New System.Drawing.Point(25, 100)
+        Me.lblAltura.Name = "lblAltura"
+        Me.lblAltura.Size = New System.Drawing.Size(88, 13)
+        Me.lblAltura.TabIndex = 5
+        Me.lblAltura.Text = "Ingrese su altura:"
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Location = New System.Drawing.Point(25, 22)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(58, 13)
+        Me.lblResultado.TabIndex = 3
+        Me.lblResultado.Text = "Resultado:"
+        '
+        'rtbResultado
+        '
+        Me.rtbResultado.Location = New System.Drawing.Point(28, 39)
+        Me.rtbResultado.Name = "rtbResultado"
+        Me.rtbResultado.ReadOnly = True
+        Me.rtbResultado.Size = New System.Drawing.Size(290, 96)
+        Me.rtbResultado.TabIndex = 4
+        Me.rtbResultado.Text = ""
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rtbResultado)
+        Me.GroupBox2.Controls.Add(Me.lblResultado)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 189)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(342, 156)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(375, 358)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Casting y Conversión de Tipos"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblAltura As Label
+    Friend WithEvents lblEdad As Label
+    Friend WithEvents lblApellido As Label
+    Friend WithEvents txtAltura As TextBox
+    Friend WithEvents txtEdad As TextBox
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents rtbResultado As RichTextBox
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ CastingConversion/MainForm.vb
@@ -0,0 +1,35 @@
+﻿Public Class MainForm
+
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        ' Variables para almacenar los datos ingresados por el usuario
+        Dim nombre As String
+        Dim apellido As String
+        Dim edad As Integer
+        Dim altura As Decimal
+
+        ' Uso de Try-Catch para manejar posibles errores al intentar convertir los datos ingresados por el usuario
+        Try
+            ' Obtener datos del usuario - Casting Implicito
+            nombre = txtNombre.Text
+            apellido = txtApellido.Text
+            edad = Integer.Parse(txtEdad.Text)
+            altura = Decimal.Parse(txtAltura.Text)
+
+            ' Operaciones con los datos
+            ' Concatenacion
+            Dim nombreCompleto As String = nombre & " " & apellido
+            ' Casting Explicito
+            Dim añoNacimiento As Integer = Date.Today.Year - edad
+            Dim alturaEnCm As Decimal = altura * 100
+
+            ' Mostrar resultados en el cuadro de texto de salida - Concatenacion
+            rtbResultado.Text = "Nombre Completo: " & nombreCompleto & " (Tipo: " & nombreCompleto.GetType().ToString() & ")" & vbCrLf &
+                                "Año de Nacimiento Aproximado: " & añoNacimiento & " (Tipo: " & añoNacimiento.GetType().ToString() & ")" & vbCrLf &
+                                "Altura en Centímetros: " & alturaEnCm & " (Tipo: " & alturaEnCm.GetType().ToString() & ")"
+
+        Catch ex As Exception
+            ' Mostrar mensaje de error si los datos ingresados no son válidos
+            MessageBox.Show("Error: Los datos ingresados no son válidos. Por favor, asegúrese de ingresar valores numéricos para la edad y la altura.", "Error de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
